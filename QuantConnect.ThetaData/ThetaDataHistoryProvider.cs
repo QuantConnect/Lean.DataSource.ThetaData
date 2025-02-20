ee01a458e8098cd1bbcffff4fb78b33dfc6e26fe
Romazes <romazes20@gmail.com>
2025-02-20 16:16:01 +0200

    Revert "feat: use max available date by subs"
    This reverts commit a1819dc960ce460824e0542794e1c18cd962bf33.

--- QuantConnect.ThetaData/ThetaDataHistoryProvider.cs
+++ QuantConnect.ThetaData/ThetaDataHistoryProvider.cs
@@ -109,15 +109,12 @@ namespace QuantConnect.Lean.DataSource.ThetaData
                 return null;
             }
 
-            var startDateTimeUtc = historyRequest.StartTimeUtc;
             if (_userSubscriptionPlan.FirstAccessDate.Date > historyRequest.StartTimeUtc.Date)
             {
                 if (!_invalidStartDateInCurrentSubscriptionWarningFired)
                 {
                     _invalidStartDateInCurrentSubscriptionWarningFired = true;
-                    Log.Trace($"{nameof(ThetaDataProvider)}.{nameof(GetHistory)}: The requested start time ({historyRequest.StartTimeUtc.Date}) exceeds the maximum available date ({_userSubscriptionPlan.FirstAccessDate.Date}) allowed by the user's subscription. Using the new adjusted start date: {_userSubscriptionPlan.FirstAccessDate.Date}.");
-                    // Ensures efficient data retrieval by blocking requests outside the user's subscription period, which reduces processing overhead and avoids unnecessary data requests.
-                    startDateTimeUtc = _userSubscriptionPlan.FirstAccessDate.Date.Add(historyRequest.StartTimeUtc.TimeOfDay);
+                    Log.Trace($"{nameof(ThetaDataProvider)}.{nameof(GetHistory)}: The requested start time ({historyRequest.StartTimeUtc.Date}) exceeds the maximum available date ({_userSubscriptionPlan.FirstAccessDate.Date}) allowed by the user's subscription.");
                 }
             }
 
@@ -131,7 +128,7 @@ namespace QuantConnect.Lean.DataSource.ThetaData
                 return null;
             }
 
-            if (startDateTimeUtc >= historyRequest.EndTimeUtc)
+            if (historyRequest.StartTimeUtc >= historyRequest.EndTimeUtc)
             {
                 if (!_invalidStartTimeWarningFired)
                 {
@@ -154,7 +151,7 @@ namespace QuantConnect.Lean.DataSource.ThetaData
             var restRequest = new RestRequest(Method.GET);
 
             restRequest = GetSymbolHistoryQueryParametersBySymbol(restRequest, historyRequest.Symbol);
-            restRequest.AddQueryParameter(RequestParameters.StartDate, startDateTimeUtc.ConvertFromUtc(TimeZoneThetaData).ConvertToThetaDataDateFormat());
+            restRequest.AddQueryParameter(RequestParameters.StartDate, historyRequest.StartTimeUtc.ConvertFromUtc(TimeZoneThetaData).ConvertToThetaDataDateFormat());
             restRequest.AddQueryParameter(RequestParameters.EndDate, historyRequest.EndTimeUtc.ConvertFromUtc(TimeZoneThetaData).ConvertToThetaDataDateFormat());
             restRequest.AddOrUpdateParameter("start_time", "0", ParameterType.QueryString);
 
@@ -174,7 +171,7 @@ namespace QuantConnect.Lean.DataSource.ThetaData
             switch (historyRequest.Resolution)
             {
                 case Resolution.Tick:
-                    return GetTickHistoryData(restRequest, historyRequest.Symbol, Resolution.Tick, historyRequest.TickType, startDateTimeUtc, historyRequest.EndTimeUtc, symbolExchangeTimeZone);
+                    return GetTickHistoryData(restRequest, historyRequest.Symbol, Resolution.Tick, historyRequest.TickType, historyRequest.StartTimeUtc, historyRequest.EndTimeUtc, symbolExchangeTimeZone);
                 case Resolution.Second:
                 case Resolution.Minute:
                 case Resolution.Hour:
