c1ee7995b80631dbcaa04c2182886e0bca319254
Romazes <romazes20@gmail.com>
2025-02-21 19:48:39 +0200

    remove: not used lib

--- QuantConnect.ThetaData/ThetaDataHistoryProvider.cs
+++ QuantConnect.ThetaData/ThetaDataHistoryProvider.cs
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
  *
@@ -24,7 +24,6 @@ using QuantConnect.Lean.Engine.HistoricalData;
 using QuantConnect.Lean.DataSource.ThetaData.Models.Rest;
 using QuantConnect.Lean.DataSource.ThetaData.Models.Common;
 using QuantConnect.Lean.DataSource.ThetaData.Models.Interfaces;
-using System.Diagnostics.Contracts;
 
 namespace QuantConnect.Lean.DataSource.ThetaData
 {
